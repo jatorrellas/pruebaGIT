65f225fa4d4e909595b0478130a86199ae90cfb7
Jesus Torrellas <j.a.torrellas@gmail.com>
2015-03-04 12:15:17 -0700

    Segundo commit con comentario aniadido

--- pruebaGitHub/src/pruebaGitHub/HelloWorld.java
+++ pruebaGitHub/src/pruebaGitHub/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// Comentario agregado de prueba de segundo commit
+		// Comentario agregado de prueba de segundo commit tomaya
 		System.out.println("Hello World!");
 
 	}
